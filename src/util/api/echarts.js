3b5ce0a49c44770b69f8f91e1b2dcf3b2b17d144
strongest-qiang <1309148358@qq.com>
2022-08-30 14:41:27 +0800

    删除所有文件

--- src/util/api/echarts.js
+++
@@ -1,7 +0,0 @@
-import service from "../index";
-export function seemURL(url) {
-  return service({
-    method: "get",
-    url: url,
-  });
-}
--- src/util/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const service = axios.create({
-  baseURL: "http://localhost:80",
-  timeout: 3000,
-});
-export default service;
